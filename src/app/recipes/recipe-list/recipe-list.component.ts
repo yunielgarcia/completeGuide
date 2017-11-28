d735a47225fa9c3f067e70730aae6f3e5b160ed5
yggarcia <garciayuni84@gmail.com>
2017-11-27 21:56:19 -0500

    first recipe part

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is a simple test', 'https://c2.staticflickr.com/6/5092/5516645415_56680c19a7_b.jpg')
+  ];
 
   constructor() { }
 
